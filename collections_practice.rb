1185e179c0ab34429622c2dd3d8dc55e94a0f421
Christopher Dutcher <cadutcher@icloud.com>
2020-06-24 00:22:52 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -31,9 +31,9 @@ def reverse_array(arr_rev)
 end
 
 def kesha_maker(arr_no_dollar)
-  arr_dollar = []
+  
   arr_no_dollar.each do |x|
-    arr_dollar << (x[2] = "$")
+    x[2] = "$"
   end
   arr_dollar
 end
